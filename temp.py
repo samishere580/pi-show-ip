4d9310c45f78b716487da7991d3eecef15d9eb8e
Samuel Barker <samuel.barker@mac19507.win2k.wkac.ac.uk>
2020-11-13 16:51:57 +0000

    hmm

--- temp.py
+++ temp.py
@@ -1,5 +1,25 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-print(s.getsockname()[0])
-s.close()
+#!/usr/bin/python
+# Example using a character LCD connected to a Raspberry Pi
+import time
+import Adafruit_CharLCD as LCD
+
+# Raspberry Pi pin setup
+lcd_rs = 25
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 18
+lcd_d7 = 22
+lcd_backlight = 2
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows = 2
+
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+while True:
+	lcd.message('Hello\nworld!')
+	# Wait 5 seconds
+
+	time.sleep(5.0)
+	lcd.clear()
